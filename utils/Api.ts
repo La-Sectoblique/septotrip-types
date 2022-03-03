67ca520729d959ba22968403807829101e84e000
Tanguy Gimenez <gimeneztanguy@gmail.com>
2022-03-03 14:39:43 +0100

    feat: adding predicates

--- utils/Api.ts
+++ utils/Api.ts
@@ -1,10 +1,26 @@
-import { EncodedSession } from "./Session";
+import { EncodedSession, isEncodedSession } from "./Session";
 
+// types
 export type ApiResponse = {
-    message: string;
+	message: string;
 }
 
 export type SuccessLoginResponse = ApiResponse & {
-    session: EncodedSession;
-    email: string;
+	session: EncodedSession;
+	email: string;
+}
+
+// types predicate
+export function isApiResponse(object: unknown): object is ApiResponse {
+	return (
+		(object as ApiResponse).message !== undefined
+	);
+}
+
+export function isSuccessLoginResponse(object: unknown): object is SuccessLoginResponse {
+	return (
+		isApiResponse(object) &&
+		isEncodedSession((object as SuccessLoginResponse).session) &&
+		(object as SuccessLoginResponse).email !== undefined
+	);
 }
--- utils/Session.ts
+++ utils/Session.ts
@@ -1,17 +1,17 @@
 export interface Session {
-    id: number,
-    username: string,
-    dateCreated: number,
-    issued: number,
-    expires: number
+	id: number,
+	username: string,
+	dateCreated: number,
+	issued: number,
+	expires: number
 }
 
 export type PartialSession = Omit<Session, "issued" | "expires">
 
 export interface EncodedSession {
-    token: string,
-    expires: number,
-    issued: number
+	token: string,
+	expires: number,
+	issued: number
 }
 
 export type DecodedSession = | { type: "valid", session: Session } | { type: "integrity-error" } | { type: "invalid-token" };
@@ -20,3 +20,14 @@ export type DecodedSession = | { type: "valid", session: Session } | { type: "in
 // grace : le token n'est plus valide mais peut être renouvellé
 // expired : le token n'est plus valide et ne peut pas être renouvellé, l'utilisateur doit se reconnecter
 export type ExpirationStatus = "expired" | "active" | "grace";
+
+
+
+// predicates
+export function isEncodedSession(object: unknown): object is EncodedSession {
+	return (
+		(object as EncodedSession).token !== undefined && 
+		(object as EncodedSession).issued !== undefined && 
+		(object as EncodedSession).expires !== undefined 
+	);
+}
