6759d07697440290640106556ab0fc61c6b097b0
Tanguy Gimenez <gimeneztanguy@gmail.com>
2022-06-10 15:40:52 +0200

    feat(Files): adding query options

--- models/File.ts
+++ models/File.ts
@@ -1,11 +1,18 @@
+import { getEnumValues } from "../utils/Enum";
 import { isVisibility, Visibility } from "../utils/Visibility";
 import { CommonAttributes, isCommonAttributes } from "./Common";
 
 // types
+export enum FileType {
+	PHOTO = "PHOTO",
+	DOCUMENT = "DOCUMENT",
+}
+
 export type FileMetadataAttributes = {
 	name: string;
 	extension: string;
 	mimeType: string;
+	fileType: FileType;
 	visibility: Visibility;
 	tempFileId?: string;
 	tripId: number;
@@ -18,12 +25,20 @@ export type FileMetadataInput = FileMetadataAttributes;
 export type FileMetadataOutput = FileMetadataAttributes & CommonAttributes;
 
 // type predicates
+export function isFileType(object: unknown): object is FileType {
+	return (
+		typeof object === "string" &&
+		getEnumValues(FileType).includes(object)
+	);
+}
+
 export function isFileMetadataAttributes(object: unknown): object is FileMetadataAttributes {
 	return (
 		(object as FileMetadataAttributes).name !== undefined && 
 		(object as FileMetadataAttributes).extension !== undefined &&
 		(object as FileMetadataAttributes).mimeType !== undefined &&
-		isVisibility((object as FileMetadataAttributes).visibility)
+		isFileType( (object as FileMetadataAttributes).fileType ) &&
+		isVisibility( (object as FileMetadataAttributes).visibility )
 	);
 }
 
