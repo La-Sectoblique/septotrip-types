bebaa657d832b940b34af820e9ddd498c5b12c34
Tanguy Gimenez <gimeneztanguy@gmail.com>
2022-06-19 23:42:51 +0200

    fix(User): adding author type

--- models/User.ts
+++ models/User.ts
@@ -11,7 +11,7 @@ export type UserInput = UserAttributes & {
 	hashedPassword: string; 
 };
 export type UserOutput = UserAttributes & CommonAttributes;
-
+export type Author = Omit<UserAttributes, "email"> & { id: number };
 
 // type predicates
 export function isUserAttributes(object: unknown): object is UserAttributes {
@@ -19,6 +19,14 @@ export function isUserAttributes(object: unknown): object is UserAttributes {
 		(object as UserAttributes).email !== undefined &&
 		(object as UserAttributes).firstName !== undefined &&
 		(object as UserAttributes).lastName !== undefined
+	);
+}
+
+export function isAuthor(object: unknown): object is Author {
+	return (
+		(object as Author).id !== undefined &&
+		(object as Author).firstName !== undefined &&
+		(object as Author).lastName !== undefined
 	);
 }
 
